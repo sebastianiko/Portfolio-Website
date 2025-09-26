68e26dd4f0e00681c1f9010accaa122539eb2c56
sebastian_iwasieczko@web.de <you@example.com>
2025-09-26 21:27:31 +0200

    editing header mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
